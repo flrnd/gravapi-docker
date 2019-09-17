e01b31ac10ccf222b6a97c583c25cd6c1dade32a
Flrn Prz <rauperez@gmail.com>
2019-09-17 11:51:58 +0200

    v0.0.3 - Add yarg for command line management and few fixes

--- src/server.js
+++ src/server.js
@@ -1,6 +1,18 @@
+import argv from 'yargs';
 import app from './app';
 
-const PORT = 5000;
+let PORT = 5000;
+const options = argv.usage('Usage: -p <PORT>').option('p', {
+  alias: 'port',
+  describe: 'server listening port',
+  type: 'integer',
+  demandOption: false,
+}).argv;
+
+// Check if env variable PORT is defined
+// to run the server on that port instead of
+// the default port.
+options.port ? (PORT = options.port) : (PORT = process.env.PORT);
 
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
