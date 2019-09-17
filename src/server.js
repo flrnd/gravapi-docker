32de578aba18e427b27ee892350579587a9ee0f3
Flrn Prz <rauperez@gmail.com>
2019-09-17 16:01:49 +0200

    v0.1.0 add command line options

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import program from 'commander';
-import { version } from '../package.json';
+import { version, description } from '../package.json';
 import app from './app';
 
 const defaultPort = 1234;
@@ -10,13 +10,35 @@ const startServer = port => {
   });
 };
 
-program
-  .version(version)
-  .command('serve [port]')
-  .alias('s')
-  .description('start the server.')
-  .action(port => {
-    port !== undefined ? startServer(port) : startServer(defaultPort);
-  });
+program.version(version, '-v, --version').description(description);
+
+program.option(
+  '-s, --serve [port]',
+  'start the server and bind to [port]',
+  value => {
+    return parseInt(value);
+  },
+  1234,
+);
+
+program.on('--help', () => {
+  console.log('');
+  console.log('  Examples:');
+  console.log(
+    '    $ node build/start.js          try to bind the server using the env PORT variable',
+  );
+  console.log(
+    '    $ node build/start.js -s       bind the server to the default port: 1234',
+  );
+  console.log(
+    '    $ node build/start.js -s 4321  bind the server to custom port: 4321',
+  );
+});
 
 program.parse(process.argv);
+
+process.env.PORT !== undefined
+  ? startServer(process.env.PORT)
+  : console.log('-h for more info');
+
+//    port !== undefined ? startServer(port) : startServer(defaultPort);
