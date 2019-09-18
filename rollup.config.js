9b00d06ba03918eb7f86ec975a550caf58ae31d8
Flrn Prz <rauperez@gmail.com>
2019-09-18 13:27:12 +0200

    add correct rollup configuration

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,22 @@
+import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+import { uglify } from 'rollup-plugin-uglify';
+import json from 'rollup-plugin-json';
+
 export default {
   input: './src/server.js',
   output: {
     file: './build/app.js',
     format: 'cjs',
-    name: 'gravapi-cli',
   },
+  plugins: [
+    resolve(),
+    commonjs(),
+    uglify(),
+    json(),
+    babel({
+      exclude: 'node_modules/**',
+    }),
+  ],
 };
